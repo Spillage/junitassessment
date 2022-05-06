0087fe5dd6644228f94272de59a16968034ccc1c
Lu Xie <pgxlxianjian@gmail.com>
2022-05-06 09:53:27 +0200

    update

--- src/test/java/com/xielu/junit-assessment/AppTest.java
+++ src/test/java/com/xielu/junit-assessment/AppTest.java
@@ -30,8 +30,7 @@ public class AppTest
 
     @Parameterized.Parameters
     public static Collection input(){
-        return Arrays.asList(new Object[][]{{"2010"},{"2011"},{"2012"},{"2013"},{"2014"},{"2015"},
-                {"2016"},{"2017"},{"2018"},{"2019"},{"2020"},{"2021"},{"shfioweh3920"}});
+        return Arrays.asList(new Object[][]{{"2010"},{"2011"},{"2012"},{"2013"},{"2020"},{"2021"},{"shfioweh3920"}});
     }
 
     @Test
@@ -39,27 +38,5 @@ public class AppTest
         String[] res = app.run(year);
         assertTrue("case fail with year " + year, res[0].equals(res[1]));
     }
-
-    /**
-    @Test
-    @DisplayName("Test in correct period")
-    public void testAPIinRightYear() throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        HttpUrl.Builder urlBuilder
-                = HttpUrl.parse(BASE_URL).newBuilder();
-        urlBuilder.addQueryParameter("drilldowns", "Nation")
-                .addQueryParameter("measures","Population")
-                .addQueryParameter("year",RIGHTYEAR);
-        String url = urlBuilder.build().toString();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        Call call = client.newCall(request);
-        Response response = call.execute();
-        JSONArray arr = JSON.parseObject(response.body().string()).getJSONArray("data");
-        assertTrue("case fail", validYear(RIGHTYEAR) && checkData(arr,RIGHTYEAR));
-    }
-    **/
-
 
 }
